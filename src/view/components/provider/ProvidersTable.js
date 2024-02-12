cc6e1e6069475e76349709a9b50fe59426b1b849
GosuUHC <GosuUchenik@yandex.ru>
2024-02-12 14:23:38 +0700

    Moved providersMapped to diff component to remove full page re-rendering

---
+++ src/view/components/provider/ProvidersTable.js
@@ -0,0 +1,70 @@
+import useProviders from "../../../viewmodel/hooks/providers/useProviders";
+import { useState } from "react";
+import ProviderStarRating from "./ProviderStarRating";
+import ProviderCardLeftPart from "./ProviderCardLeftPart";
+import ProviderCardMiddlePart from "./ProviderCardMiddlePart";
+import ProviderCardRightPart from "./ProviderCardRightPart";
+import ProviderCard from "./ProviderCard";
+import { Stack } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import { CaretDownFill } from "react-bootstrap-icons";
+
+const ProvidersTable = () => {
+  const { providersData, isSuccess } = useProviders();
+  const [limit, setLimit] = useState(3);
+
+  if (!isSuccess) {
+    return;
+  }
+
+  const onShowMore = () => {
+    setLimit(limit + 3);
+  };
+
+  const providersMapped = providersData.slice(0, limit).map((provider, i) => {
+    const rating = (
+      <ProviderStarRating precision={0.1} value={provider.rating_overall} />
+    );
+
+    const left = (
+      <ProviderCardLeftPart
+        imgSrc={provider.image}
+        name={provider.name}
+        component={rating}
+      />
+    );
+
+    const middle = (
+      <ProviderCardMiddlePart
+        minPrice={provider.min_price}
+        maxSpeed={provider.max_internet_speed_value}
+        maxSpeedUnits={provider.max_internet_speed_units}
+        plansCount={provider.plans_count}
+        promotionsCount={provider.promotions_count}
+        reviewsCount={provider.reviews_count}
+      />
+    );
+
+    const right = (
+      <ProviderCardRightPart
+        description={provider.description}
+        phone={provider.phone}
+      />
+    );
+
+    return <ProviderCard key={i} components={[left, middle, right]} />;
+  });
+
+  return (
+    <Stack gap={3} className="pb-3">
+      {providersMapped}
+      {limit < providersData.length && (
+        <Button as="div" bsPrefix="show" onClick={onShowMore}>
+          <CaretDownFill /> Показать больше
+        </Button>
+      )}
+    </Stack>
+  );
+};
+
+export default ProvidersTable;
--- src/view/pages/providers/ProvidersPage.js
+++ src/view/pages/providers/ProvidersPage.js
@@ -1,66 +1,12 @@
 import Container from "react-bootstrap/Container";
 import AddressWithText from "../../components/address/AddressWithText";
-import ProviderStarRating from "../../components/provider/ProviderStarRating";
-import ProviderCardLeftPart from "../../components/provider/ProviderCardLeftPart";
-import ProviderCard from "../../components/provider/ProviderCard";
-import ProviderCardMiddlePart from "../../components/provider/ProviderCardMiddlePart";
-import ProviderCardRightPart from "../../components/provider/ProviderCardRightPart";
-import { Row, Stack } from "react-bootstrap";
-import useProviders from "../../../viewmodel/hooks/providers/useProviders";
+import { Row } from "react-bootstrap";
 import ProvidersSorting from "../../components/provider/ProvidersSorting";
-import { useState } from "react";
-import Button from "react-bootstrap/Button";
-import { CaretDownFill } from "react-bootstrap-icons";
 import CallBackInfoCard from "../../components/common/callBackInfoCard/CallBackInfoCard";
+import ProvidersTable from "../../components/provider/ProvidersTable";
+import { memo } from "react";
 
 const ProvidersPage = () => {
-  const { providersData, isSuccess } = useProviders();
-  const [limit, setLimit] = useState(3);
-
-  const onShowMore = () => {
-    setLimit(limit + 3);
-  };
-
-  if (!isSuccess) {
-    return;
-  }
-
-  const providersMapped = providersData.slice(0, limit).map((provider, i) => {
-    const rating = (
-      <ProviderStarRating precision={0.1} value={provider.rating_overall} />
-    );
-
-    const left = (
-      <ProviderCardLeftPart
-        imgSrc={provider.image}
-        name={provider.name}
-        component={rating}
-      />
-    );
-
-    const middle = (
-      <ProviderCardMiddlePart
-        minPrice={provider.min_price}
-        maxSpeed={provider.max_internet_speed_value}
-        maxSpeedUnits={provider.max_internet_speed_units}
-        plansCount={provider.plans_count}
-        promotionsCount={provider.promotions_count}
-        reviewsCount={provider.reviews_count}
-      />
-    );
-
-    const right = (
-      <ProviderCardRightPart
-        description={provider.description}
-        phone={provider.phone}
-      />
-    );
-
-    return (
-      <ProviderCard key={i} components={[left, middle, right]}></ProviderCard>
-    );
-  });
-
   return (
     <div className="mainWrapper">
       <Container>
@@ -68,14 +14,7 @@ const ProvidersPage = () => {
           <AddressWithText />
         </Row>
         <ProvidersSorting />
-        <Stack gap={3} className="pb-3">
-          {providersMapped}
-          {limit < providersData.length && (
-            <Button as="div" bsPrefix="show" onClick={onShowMore}>
-              <CaretDownFill /> Показать больше
-            </Button>
-          )}
-        </Stack>
+        <ProvidersTable />
       </Container>
       <div className="py-5 callBackInfoWrapper">
         <Container>
@@ -85,5 +24,4 @@ const ProvidersPage = () => {
     </div>
   );
 };
-
-export default ProvidersPage;
+export default memo(ProvidersPage);
