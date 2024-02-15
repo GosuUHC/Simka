5158d7a07ac5646dd2f512e1ca433b4921d0a768
GosuUHC <GosuUchenik@yandex.ru>
2024-02-15 12:46:21 +0700

    application form cosmetics

--- src/view/components/application/ApplicationFormInputs.js
+++ src/view/components/application/ApplicationFormInputs.js
@@ -1,12 +1,17 @@
-import { Stack } from "react-bootstrap";
+import { Col, Stack } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
 const ApplicationFormInputs = () => {
   return (
-    <Stack direction="horizontal" gap={2}>
-      <Form.Control placeholder="Введите ваше имя" />
-      <Form.Control type="phone" placeholder="Введите ваш телефон" />
+    <Stack className="py-2" direction="horizontal" gap={2}>
+      <Col xs="auto">
+        <Form.Control placeholder="Введите ваше имя" />
+      </Col>
+      <Col xs="auto">
+        <Form.Control type="phone" placeholder="Введите ваш телефон" />
+      </Col>
+
       <Button variant="secondary">Подобрать тариф</Button>
     </Stack>
   );
